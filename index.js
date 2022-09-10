ecb50e16448c593bfd09738ba0f08ca3bc1f073f
LuCe2022 <113069166+LuCe2022@users.noreply.github.com>
2022-09-10 15:33:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let pais = "Italia";
+let continente = "Europa";
+let antiguedad = "2022";
+let paisContinente = pais + " " + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(paisContinente);
